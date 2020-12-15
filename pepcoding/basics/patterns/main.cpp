0b9d54686b9603173b21615229bf12573507f405
akshayashu <akshay.khanna0810@gmail.com>
2020-12-15 22:57:49 +0530

    any base multiplication

--- pepcoding/basics/patterns/main.cpp
+++ pepcoding/basics/patterns/main.cpp
@@ -1,43 +1,74 @@
-#include <bits/stdc++.h>
-#define ll long long
-
+#include<bits/stdc++.h>
 using namespace std;
 
-int max(vector<int>& a){
+int addition(int n1, int n2, int b){
+    int i=0, final = 0, carry = 0, power = 1;
     
-    int max = -1e8;
-    for(int i=0;i<a.size();i++){
-        if(a[i] > max){
-            max= a[i];
-        }
+    while(n1!=0 || n2!=0 || carry !=0){
+        int r1 = n1%10;
+        int r2 = n2%10;
+        
+        int sum =r1+r2+carry;
+        
+        n1/=10;
+        n2/=10;
+        
+        int rem = sum%b;
+        carry = sum/b;
+        
+        final += rem * power;
+        power *= 10;
     }
-    
-    return max;
+    return final;
 }
 
-int min(vector<int>& a){
-    
-    int min = 1e8;
-    for(int i=0;i<a.size();i++){
-        if(a[i] < min){
-            min = a[i];
-        }
+
+int multiply(int n, int q, int power, int b){
+    int ans = 0, carry = 0;
+    cout<<"\n\n";
+    while(n!=0 || carry!=0){
+        int m2 = n%10;
+        int res = m2*q+carry;
+        cout<<m2<<" x "<<q<<" + "<<carry<< " = "<<res<<endl;
+        
+        n/=10;
+        
+        int rem = res%b;
+        carry = res/b;
+        
+        ans += rem*power;
+    	cout<<ans<<"\n";
+        power *=10;
     }
+    return ans;
+}
+
+int anyBaseSub(int n2, int n1, int b){
+    int i=0, ans = 0, power = 1, power1 = 1, res = 0 ;
+    int a1=0,a2=0;
     
-    return min;
+    while(n1!=0 ){
+        
+        int r1 = n1%10;
+        
+        a2 = multiply(n2, r1, power1, b);
+        res = addition(a1,a2, b);
+        a1=res;
+
+        
+        n1 /=10;
+        power1*=10;
+    }
+    return res;
 }
 
 int main(){
-	
-	int n;
-	cin>>n;
-	vector<int> a(n);
-
-	for(int i=0; i<n;i++){
-		cin>>a[i];
-	}
-	
-	cout<<max(a)-min(a);
-
-	return 0;
+    
+    int n1, n2, b;
+    cin>>b>>n1>>n2;
+    
+    cout<<anyBaseSub(n1, n2, b);
+    
+    return 0;
+    
 }

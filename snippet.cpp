1bfb5bc939c01073bd78342a9a8a9413971c0539
akkhanna <akkhanna@redhat.com>
2022-04-30 17:10:15 +0530

    added snippet

---
+++ snippet.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+
+void solve(string str, int n){
+    
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+ 
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+ 
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+
+        int n;
+        cin >> n;
+
+        cout << solve( , n) << endl;
+    }
+
+    return 0;
+}
